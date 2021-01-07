f32c6228d739738fccc958c0fb2d80960c5ed11e
jajaceek <59606897+jajaceek@users.noreply.github.com>
2021-01-07 13:42:35 +0100

    Add files via upload
    kompletna mechanika

--- coffeeMachine.rb
+++ coffeeMachine.rb
@@ -9,6 +9,7 @@ class CoffeeMachine
     @choice = ""
     @wellcoming1 = "Write action (buy, fill, take, remaining, exit): "
     @wellcoming2 = "Write action (buy, fill, take, remaining, exit): "
+    @fill = 0
   end
 
   def print_amount
@@ -33,8 +34,9 @@ class CoffeeMachine
         puts "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: "
         @wellcoming1 = ""
       when @choice == "back"
-        puts "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: "
+          puts "#{@wellcoming2}"
 
+        # zakup kawy
       when @choice == "1"
         if @water_status < 250
           puts "Sorry, not enough water!"
@@ -56,6 +58,87 @@ class CoffeeMachine
           print_amount
         end
 
+      when @choice == "2"
+        if @water_status < 350
+          puts "Sorry, not enough water!"
+          puts "#{@wellcoming2}"
+        elsif @milk_status < 75
+          puts "Sorry, not enough milk!"
+          puts "#{@wellcoming2}"
+        elsif @coffee_beans_status < 20
+          puts "Sorry, not enough coffee!"
+          puts "#{@wellcoming2}"
+        elsif @disposable_cup_status < 1
+          puts "Sorry, not enough cups!"
+          puts "#{@wellcoming2}"
+        else
+          @water_status -= 350
+          @milk_status -= 75
+          @coffee_beans_status -= 20
+          @disposable_cup_status -= 1
+          @money_status += 7
+          puts "I have enough resources, making you a coffee!"
+          puts "Your latte is ready"
+          puts "Write action (buy, fill, take, remaining, exit): "
+          print_amount
+        end
+
+      when @choice == "3"
+        if @water_status < 200
+          puts "Sorry, not enough water!"
+          puts "#{@wellcoming2}"
+        elsif @milk_status < 100
+          puts "Sorry, not enough milk!"
+          puts "#{@wellcoming2}"
+        elsif @coffee_beans_status < 12
+          puts "Sorry, not enough coffee!"
+          puts "#{@wellcoming2}"
+        elsif @disposable_cup_status < 1
+          puts "Sorry, not enough cups!"
+          puts "#{@wellcoming2}"
+        else
+          @water_status -= 200
+          @milk_status -= 100
+          @coffee_beans_status -= 12
+          @disposable_cup_status -= 1
+          @money_status += 6
+          puts "I have enough resources, making you a coffee!"
+          puts "Your cappuccino is ready"
+          puts "Write action (buy, fill, take, remaining, exit): "
+          print_amount
+        end
+
+        # uzupełnianie zasobów
+      when @choice == "fill"
+        puts "Write how many ml of water do you want to add: "
+        @fill = gets.chomp.to_i
+        puts "#{@water_status}"
+        @water_status += @fill
+        puts "#{@water_status}"
+        puts "Write how many ml of milk do you want to add: "
+        @fill = gets.chomp.to_i
+        puts "#{@milk_status}"
+        @milk_status += @fill
+        puts "#{@milk_status}"
+        puts "Write how many grams of coffee beans do you want to add: "
+        @fill = gets.chomp.to_i
+        puts "#{@coffee_beans_status}"
+        @coffee_beans_status += @fill
+        puts "#{@coffee_beans_status}"
+        puts "Write how many disposable cups of coffee do you want to add: "
+        @fill = gets.chomp.to_i
+        puts "#{@disposable_cup_status}"
+        @disposable_cup_status += @fill
+        puts "#{@disposable_cup_status}"
+
+      when @choice == "take"
+        puts "#{@money_status}"
+        puts "I gave you $#{@money_status}"
+        @money_status = 0
+        puts "#{@money_status}"
+
+      when @choice == "remaining"
+        print_amount
       end
     end
 
